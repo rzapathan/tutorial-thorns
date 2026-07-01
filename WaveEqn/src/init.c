e5f04d4ff8397f7546791917a0194e15ba6cec16
crab <owner@owners-MacBook-Pro.local>
2026-07-01 16:30:01 -0700

    -_-

--- WaveEqn/src/init.c
+++ WaveEqn/src/init.c
@@ -3,22 +3,21 @@
 #include <cctk_Parameters.h>
 
 // Initialize grid functions
-
 void WaveEqn_Initialize(CCTK_ARGUMENTS)
 {
-    DECLARE_CCTK_ARGUMENTS_WaveEqn_Initialize; // declare all grid fns from interface.ccl
-    DECLARE_CCTK_PARAMETERS; // declare all params from param.ccl
+  DECLARE_CCTK_ARGUMENTS_WaveEqn_Initialize;  // Declare all grid functions from interface.ccl
+  DECLARE_CCTK_PARAMETERS; // Declare all parameters from param.ccl
 
-    for (int k = 0; k < cctk_lsh[2]; k++) // loop over the z dir
+  for (int k = 0; k < cctk_lsh[2]; k++) // loop over the z direction
+  {
+    for (int j = 0; j < cctk_lsh[1]; j++) // loop over the y direction
     {
-        for (int j = 0; j < cctk_lsh[1]; j++) // y dir
-        {
-            for (int i = 0; i < cctk_lsh[1]; i++) // x dir
-            {
-                const size_t ijk = CCTK_GFINDEX3D(cctkGH, i, j, k);
-                U[ijk] = exp( -x[ijk] * x[ijk] - y[ijk] * y[ijk]);
-                K[ijk] = -0.0;
-            }
-        }
+      for (int i = 0; i < cctk_lsh[0]; i++) // loop over the x direction
+      {
+        const size_t ijk = CCTK_GFINDEX3D(cctkGH, i, j, k);
+        U[ijk] = exp(-x[ijk] * x[ijk] - y[ijk] * y[ijk]);
+        K[ijk] = 0.0;
+      }
     }
+  }
 }
