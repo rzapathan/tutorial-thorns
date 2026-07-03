d0659543d56d3a4ea8ce8bdf8549e972628938de
crab <owner@owners-MacBook-Pro.local>
2026-07-03 14:12:25 -0700

    a bit excessive

--- WaveEqn/src/mol_register.c
+++ WaveEqn/src/mol_register.c
@@ -2,29 +2,32 @@
 #include <cctk_Arguments.h>
 #include <cctk_Parameters.h>
 
-
-// Register variables w MoL
-void WaveEqn_MolRegister(CCTK_ARGUMENTS)
+// Register variables with MoL
+void WaveEqn_MoLRegister(CCTK_ARGUMENTS)
 {
-    DECLARE_CCTK_ARGUMENTS_WaveEqn_MoLRegister; //declaring grid fns from interface
-    CCTK_INT ierr = 0, group, rhs;
+  DECLARE_CCTK_ARGUMENTS_WaveEqn_MoLRegister;  // Declare all grid functions from interface.ccl
+  CCTK_INT ierr = 0, group, rhs;
 
-    // Look up at the group index of the evol_group
-    rhs = CCTK_GroupIndex ("WaveEqn::rhs_group");
+  // Look up the group index of the evol_group
+  group = CCTK_GroupIndex ("WaveEqn::evol_group");
+  
+  // Look up the group index of the rhs_group
+  rhs = CCTK_GroupIndex ("WaveEqn::rhs_group");
 
-    if (CCTK_IsFunctionAliased ("MoLRegisterEvolvedGroup"))
-    {
-        // *** key step *** tell MoL what vars we're evolving & what rhs vars are
-        ierr += MoLRegisterEvolvedGroup(group,rhs);
-    }
-    else 
-    {
-        CCTK_WARN (0, "MoL function not aliased !");
-        ierr++;
-    }
+  if (CCTK_IsFunctionAliased ("MoLRegisterEvolvedGroup"))
+  {
+    // *** KEY STEP! *** Tell MoL what we variables
+    // we are evolving and what the rhs variables are.
+    ierr += MoLRegisterEvolvedGroup(group, rhs);
+  }
+  else
+  {
+    CCTK_WARN (0, "MoL function not aliased !");
+    ierr++;
+  }
 
-    if (ierr) {
-        CCTK_WARN(CCTK_WARN_ABORT, "Problems registering with MoL");
-    }
-
+  if (ierr)
+  {
+    CCTK_WARN(CCTK_WARN_ABORT, "Problems registering with MoL !");
+  }
 }
